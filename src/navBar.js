63f4f321c6594509f14dab4d935c9eb859f02d10
meggouri ismail <meggouri.ismail@gmail.com>
2021-04-03 00:41:24 +0100

    nav-bar mobile friendly

--- src/navBar.js
+++ src/navBar.js
@@ -6,22 +6,32 @@ function NavBar() {
         } else {
             document.querySelector('.navBar').classList.remove('navBar-sticky');
         }
-    })
+    });
+    
+    function showLinks() {
+        const navlinks = document.querySelector('.nav-links');
+        navlinks.classList.toggle('nav-links-active');
+        navlinks.classList.add('nav-links-display');
+        
+    }
+
     return (
         <div className="navBar">
             <h2>Ecommerce</h2>
-            <ul className="page-links">
-                <li>Home</li>
-                <li>Shop</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            <div className="nav-links">
+                <ul className="page-links">
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
             <ul className="card-links">
                 <li><img src="/assets/icons/search.png" alt="like"/></li>
                 <li><img src="/assets/icons/shopping_cart.png" alt="shopping"/></li>
                 <li><img src="/assets/icons/love.png" alt="like"/></li>
             </ul>
-            <img src="/assets/icons/menu.png" alt="like"/>
+            <img src="/assets/icons/menu.png" alt="like" className="menu-sm" onClick={showLinks} />
         </div>
     );
 }
