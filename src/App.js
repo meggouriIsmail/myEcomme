e5a7858d5dbbcd50e4a104c8db691280ee6b0670
meggouri ismail <meggouri.ismail@gmail.com>
2021-04-02 22:51:11 +0100

    adding components such as products

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Categories from './Categories';
 import Hero from './Hero';
 import NavBar from './navBar'
+import Products from './Products';
+import ProductsData from './products.json';
+import Footer from './Footer';
 
 function App() {
+  let [products, setProducts] = useState('');
+
+  useEffect(() => {
+    const data = ProductsData;
+    setProducts(data.main.products);
+  }, [setProducts]);
+
+
   return (
-    <div className="App">
+    <div className="App" onScroll={(e) => console.log(e)}>
       <NavBar/>
       <Hero/>
       <Categories/>
-      <div style={{height:"300px"}}></div>
+      <Products info={products} />
+      <Footer/>
     </div>
   );
 }
